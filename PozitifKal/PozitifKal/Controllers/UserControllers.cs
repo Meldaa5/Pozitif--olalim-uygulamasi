dccc55f5f035b927a064190e35f0b9e5824ac9ed
Meldaa5 <78598507+Meldaa5@users.noreply.github.com>
2024-04-07 08:56:14 +0300

    register and login writed
    register and login writed in .net

---
+++ PozitifKal/PozitifKal/Controllers/UserControllers.cs
@@ -0,0 +1,54 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PozitifKal.Data;
+using PozitifKal.Model;
+
+namespace PozitifKal.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserControllers:ControllerBase
+{   
+    private readonly AppDbContext appDbContext;
+    public UserControllers(AppDbContext appDbContext)
+    {
+        this.appDbContext = appDbContext;
+    }
+    [HttpPost]
+    public async Task<IActionResult> AddUser(User user)
+    {
+        appDbContext.Users.Add(user);
+        await appDbContext.SaveChangesAsync();
+        return Ok(user);
+    }
+    [HttpGet]
+    public async Task<IActionResult> getUsers()
+    {
+        var users = await appDbContext.Users.ToListAsync();
+        return Ok(users);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserById(int id)
+    {
+        var user = await appDbContext.Users.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound(); // Kullanıcı bulunamadıysa 404 Not Found döndür
+        }
+
+        return Ok(user); // Kullanıcıyı bulduğumuzda 200 OK döndür
+    }
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginUser(string email, string password)
+    {
+        var user = await appDbContext.Users.FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+
+        if (user == null)
+        {
+            return Unauthorized(); // Kullanıcı kimlik doğrulaması başarısızsa 401 Unauthorized döndür
+        }
+
+        return Ok(user); // Kullanıcı kimlik doğrulaması başarılıysa 200 OK döndür
+    }
+}
---
+++ PozitifKal/PozitifKal/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PozitifKal.Model;
+
+namespace PozitifKal.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+        //public DbSet<Admin> Admins { get; set; }
+    }
+}
